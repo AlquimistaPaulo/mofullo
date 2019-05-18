cc82eb8354f7a68e4fd782952eba7ed42d0f0c5b
Shinya-Omagari <fukuoka.tokyo.shiga@gmail.com>
2019-05-18 18:15:47 +0900

    20190512153131_create_tasks.rbのファイルを削除しました。

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  end
+
+end
